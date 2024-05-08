460d32cb411da75eae88c1ad8b1e0ae7f287b385
Kuba <maciazek.jakub@gmail.com>
2024-05-08 11:19:27 +0200

    Dodanie buta

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+var schoesIcon = L.icon({
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/20/Football_shoe.svg',
+	iconSize:     [100, 60]
+	
+});
+
+var marker = L.marker([52.225,21.005], {icon: schoesIcon}).addTo(map);
