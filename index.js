5594abe973671baac9ca04d864dd1a201c2eb9b0
Kuba Maciążek <126525650+JakMac02@users.noreply.github.com>
2024-05-10 11:19:45 +0200

    maoka

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([52.222, 21.01], 16);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://www.maptiler.com/maps/#style=openstreetmap&mode=2d&position={z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
