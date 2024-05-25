d6dc44acbb4059dba348305bbdced998f7262c06
Kuba <maciazek.jakub@gmail.com>
2024-05-25 15:09:46 +0200

    Dodanie przcisku 'Dodaj ogłoszenia' i poprawki

--- smieciarka-website/js/main.js
+++ smieciarka-website/js/main.js
@@ -26,118 +26,6 @@ var cupboardIcon = L.icon({
 
 let buty = "Buty"
 
-var offers = [
-	{
-      "type": "Feature",
-      "properties": {
-        "category": "clothes",
-        "description": "Chcę oddać stare buty. Są w niezłym stanie",
-        "photos": "IWNIOIWND898YTV97QEH380V23H8",
-        "title": "Buty",
-        "term": "Jak najszybciej",
-		"author": "Jan Kowalski"
-      },
-      "geometry": {
-        "coordinates": [
-          21.005, 52.225
-        ],
-        "type": "Point"
-      }
-    },
-	{
-      "type": "Feature",
-      "properties": {
-        "category": "clothes",
-        "description": "Chcę oddać prawie nową koszulę. Jest w super stanie",
-        "photos": "IWNIOIWND898YTV97QEH380V23H8",
-        "title": "Koszulka",
-        "term": "Jak najszybciej",
-		"author": "Adam Nowak"
-      },
-      "geometry": {
-        "coordinates": [
-          21.008,
-		  52.2246
-        ],
-        "type": "Point"
-      }
-    },
-	{
-      "type": "Feature",
-      "properties": {
-        "category": "furniture",
-        "description": "Chcę oddać starą szafę. Jest w niezłym stanie. Jest w akceptowalnym stanie. Jej wysokość to 3 metry, jest duża :))",
-        "photos": "IWNIOIWND898YTV97QEH380V23H8",
-        "title": "Szafa",
-        "term": "Jak najszybciej",
-		"author": "Janusz Nowakowski"
-      },
-      "geometry": {
-        "coordinates": [
-			21.01026,
-			52.22057
-			
-        ],
-        "type": "Point"
-      }
-    },
-	{
-      "type": "Feature",
-      "properties": {
-        "category": "furniture",
-        "description": "Hi everyone! I really want to get rid of that cupboard. Recently, I have bought the new one. So, please, take it to your house:)",
-        "photos": "IWNIOIWND898YTV97QEH380V23H8",
-        "title": "Old cupboard",
-        "term": "Jak najszybciej",
-		"author": "John Doe"
-      },
-      "geometry": {
-        "coordinates": [
-			21.00963,
-			52.222573
-			
-        ],
-        "type": "Point"
-      }
-    },
-	{
-      "type": "Feature",
-      "properties": {
-       "category":"furniture",
-	   "description":"Metr osiemdziesiąt, 3 szuflady",
-	   "photos":"OWJPFEJF28902HF789H28IHF0293",
-	   "title":"wysoka szuflada",
-	   "term":"jak najszybciej",
-	   "author":"Adam Małsysz",
-      },
-      "geometry": {
-        "coordinates": [
-			21.000,  
-			52.230			
-        ],
-        "type": "Point"
-      }
-    },
-	{
-	"type": "Feature",
-      "properties": {
-       "category":"clothes",
-	   "description":"lorem ipsum",
-	   "photos":"IWNIOIWND898YTV97QEH380V23H8",
-	   "title":"szybkie buty marki zygzak mcqueen",
-	   "term":"jak najszybciej",
-	   "author":"Robert Kubica",
-      },
-      "geometry": {
-        "coordinates": [
-			21.005, 
-			52.225
-        ],
-        "type": "Point"
-      }
-    },
-]
-
 for (i in offers) {
 	offer = offers[i];
 	var icon;
@@ -158,10 +46,10 @@ for (i in offers) {
 			offer.properties.title + '<br>'
 			+ 'Opis ' + '<br>' + offer.properties.description + '<br>'
 			+ 'Termin odbioru ' + '<br>' + offer.properties.term + '<br>'
-			+ 'Autor<br>' + offer.properties.author + '<br><a onclick="slideBar(' + "'" 
+			+ 'Autor<br>' + offer.properties.author + '<br><a onclick="answerToTheOffer(' + "'" 
 			+ offer.properties.title.toString() + "'" + ',' + "'" + offer.properties.author.toString() + "'" 
 			+ ',' + "'" + offer.properties.description.toString() + "'" + ') ">'
-			+ "<button class=\"button\">Odbierz</button> </a></center>" 
+			+ "<button class=\"button\" id=\"buttonGet\">Odbierz</button> </a></center>" 
 	);
 };
 
@@ -170,11 +58,18 @@ function onEachFeature(feature, layer) {
 };
 
 
-async function slideBar(Title, Author, Description) {
-	document.getElementById("sidebar").style.width = "20%";
+async function answerToTheOffer(Title, Author, Description) {
+	document.getElementById("leftsidebar").style.width = "20%";
 	document.getElementById("map").style.marginLeft= "20%";
 	document.getElementById('Title').innerText = Title;
 	document.getElementById('Author').innerText = Author;
 	document.getElementById('Description').innerText = Description;
 	console.log(Title, Author, Description)
+};
+
+function addOffer() {
+    console.log("Dodaj Ogłoszenie!");
+	document.getElementById("rightsidebar").style.width = "20%";
+	document.getElementById("map").style.marginRight= "20%";
+	document.getElementById("buttonAdd").style.visibility= "hidden";
 };
